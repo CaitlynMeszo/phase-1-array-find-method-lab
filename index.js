c01543829b8fcc8f4e7d3dfd3e9631fbbfe4a461
Caitlyn <meszaros.caitlyn@gmail.com>
2021-06-17 22:05:14 -0400

    commit tree 4527722fcfa098250ee625a36ff5409e7cbcd417

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function superbowlWin (record) {
 
+    const winner = record.find(r => r.result === "W")
+    if (winner) {
+        return winner.year
+    } else {
+        return undefined
+    }
+    
+}
